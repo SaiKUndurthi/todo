976e381bd3ce84c26eaa270220fd2eb42365aab3
sai-u <sai.undurthi@gmail.com>
2020-05-24 11:35:12 -0700

    Rendering the todo list from the DB.

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from env import dbstring
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=dbstring #"postgres://user:password@localhost:5432/todoapp"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+# todo1 = Todo(description="Test entry to the database")
+# db.session.add(todo1)
+#
+# Todo.query.first()
 @app.route("/")
 def index():
-    return render_template("index.html", data=[
-    {
-    'description':'Todo 1'
-    },
-    {
-    'description':'Todo 2'
-    },
-    {
-    'description':'Todo 2'
-    }])
+    return render_template("index.html", data=Todo.query.all())
